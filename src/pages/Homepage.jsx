1508b61b8d72eae704caeb0303e0923eb5d7f2e0
Aleksandar Staykov <apsreat@gmail.com>
2023-10-08 15:15:44 +0200

    added the led controll with api calls

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,39 +1,73 @@
 import styles from "./Homepage.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { HexAlphaColorPicker } from "react-colorful";
 
+import { debounce } from "lodash";
+import Spinner from "../components/Spinner";
+
+const urlBase = "http://192.168.1.55:80";
+
 function Homepage() {
-  const [brightness, setBrightness] = useState(255);
-  const [color, setColor] = useState("#fff");
   const [isOn, setIsOn] = useState(false);
+  const [color, setColor] = useState("#FFF");
+  const [brightness, setBrightness] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
-  function handleOnToggle() {
+  useEffect(() => {
+    const fetchLightStatus = () =>
+      fetch(`${urlBase}/isOn`)
+        .then((res) => res.text())
+        .then((data) => setIsOn(data === "1"));
+
+    const fetchLightColor = () =>
+      fetch(`${urlBase}/getColor`)
+        .then((res) => res.text())
+        .then((data) => setColor(data));
+
+    const fetchLightBrightness = () =>
+      fetch(`${urlBase}/getBrightness`)
+        .then((res) => res.text())
+        .then((data) => setBrightness(Number(data)));
+
+    Promise.all([
+      fetchLightStatus(),
+      fetchLightColor(),
+      fetchLightBrightness(),
+    ]).then(() => setIsLoading(false));
+  }, []);
+
+  function handleToggle() {
     setIsOn((prev) => !prev);
+    fetch(`${urlBase}/toggle`);
   }
 
-  function hanldeNewCollor(alfaHexValue) {
-    const colorHexValue = alfaHexValue.slice(0, 7);
-    const newBrightness = parseInt(alfaHexValue.slice(7, 9) || "ff", 16);
+  const handleColorChange = debounce((newColor) => {
+    const colorValue = newColor.slice(1, 7);
+    const brightnessValue = parseInt(newColor.slice(7) || "ff", 16);
 
-    if (newBrightness !== brightness) {
-      setBrightness(newBrightness);
-      console.log(newBrightness);
+    if (brightnessValue !== brightness) {
+      setBrightness(brightnessValue);
+      fetch(`${urlBase}/setBrightness?value=${brightnessValue}`);
     }
 
-    if (color !== colorHexValue) {
-      setColor(colorHexValue);
-      console.log(colorHexValue);
+    if (colorValue !== color) {
+      setColor(colorValue);
+      fetch(`${urlBase}/setColor?value=${colorValue}`);
     }
-  }
+    setIsOn(true);
+  }, 500);
 
   const buttonType = isOn ? "onButton" : "offButton";
+  const computedColor = `${color}${brightness.toString(16).padStart(2, "0")}`;
 
   return (
     <div className={styles.homepage}>
       <h1>Alarm homepage</h1>
 
-      <HexAlphaColorPicker color={color} onChange={hanldeNewCollor} />
-      <button className={styles[buttonType]} onClick={handleOnToggle}>
+      {isLoading ?? <Spinner />}
+
+      <HexAlphaColorPicker color={computedColor} onChange={handleColorChange} />
+      <button className={styles[buttonType]} onClick={handleToggle}>
         Toggle
       </button>
     </div>
